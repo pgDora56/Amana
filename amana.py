3c1b32d5f5977a11ef299c826875decf5cc83753
Kazuki F <doradora.prog@gmail.com>
2021-05-04 17:52:18 +0900

    Modify auto-remove and apply logging-package

--- amana.py
+++ amana.py
@@ -2,7 +2,15 @@
 import discord
 import json
 import re
+import asyncio
+import logging
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s- %(name)s - %(levelname)s - %(message)s'
+)
+
+remove_check_channel = []
 client = discord.Client()
 with open('config.json') as f:
     jsdata = json.load(f)
@@ -11,7 +19,8 @@ token = jsdata['token']
 
 @client.event
 async def on_ready():
-    print("Amana: On ready.")
+    logging.info("On ready.")
+    await remove_check()
 
 
 @client.event
@@ -21,8 +30,7 @@ async def on_message(message):
         return
 
     cmds = message.content.split()
-    print(cmds)
-    print(client.user in cmds)
+    logging.debug(cmds)
     delete = []
     for c in cmds:
         m = re.fullmatch(r"<@![0-9]+>", c)
@@ -36,7 +44,7 @@ async def on_message(message):
 
 
 async def reply(msg, cmds):
-    reply = "甜花ちゃんどこ？"
+    reply = "???"
     if cmds[0] == "room":
         if len(cmds) == 2:
             await make_channel(cmds[1])
@@ -45,16 +53,57 @@ async def reply(msg, cmds):
 
 
 async def make_channel(name):
-    global jsdata
+    global jsdata, remove_check_channel
     guild = client.get_guild(jsdata["target-guild"])
     cate = client.get_channel(jsdata["target-category"])
-    await guild.create_voice_channel(name, category=cate)
+    ch = await guild.create_voice_channel(name, category=cate)
+    remove_check_channel.append(TemporaryChannel(ch))
 
 
-@client.event
-async def on_voice_state_update(member, before, after):
-    # Voiceの状況が変わったときに呼び出される
-    # チャンネル移動やミュートの解除など
-    pass
+class TemporaryChannel:
+    def __init__(self, channel):
+        self.ch = channel
+        self.remain = 0
 
-client.run(token)
+    async def check(self):
+        global jsdata
+        try:
+            len_member = len(self.ch.members)
+            if len_member == 0:
+                self.remain += 1
+                if self.remain >= jsdata["wait-remove"]:
+                    await self.delete()
+                    return True
+            else:
+                self.remain = 0
+            return False
+        except Exception as e:
+            logging.error(f"Fatal error and ended watching: {e}")
+            return True
+
+    async def delete(self):
+        d_reason = f"Deleted over time({jsdata['wait-remove']})"
+        await self.ch.delete(reason=d_reason)
+
+    def string(self):
+        return f"TempCh: {self.ch} {self.remain}"
+
+
+async def remove_check():
+    global jsdata, remove_check_channel
+    while True:
+        await asyncio.sleep(60)
+
+        logging.info("Remove Check")
+        deleted = []
+        for idx in range(len(remove_check_channel)):
+            isdeleted = await remove_check_channel[idx].check()
+            if isdeleted:
+                deleted.insert(0, idx)
+            logging.debug(remove_check_channel[idx].string())
+
+        for dch in deleted:
+            remove_check_channel.pop(dch)
+
+if __name__ == "__main__":
+    client.run(token)
